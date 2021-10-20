78d4c7b7074369fe685ee5d5a6ba8e49fc129706
Ezra Fielding <ezra.fielding@gmail.com>
2021-10-20 14:15:58 +0200

    Completed base autoencoder structure

--- unsupervised_learning/galaxyencode.py
+++ unsupervised_learning/galaxyencode.py
@@ -1,21 +1,36 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras import layers
+from tensorflow.python.keras.layers.convolutional import Conv2DTranspose, UpSampling2D
 
 class GalaxyEncoder(Model):
     def __init__(self):
         super(GalaxyEncoder, self).__init__()
         self.encoder = tf.keras.Sequential ([
-            layers.Input(shape=(14400,1)),
-            layers.Conv1D(1, 25, activation=tf.nn.leaky_relu, strides=2),
-            layers.Conv1D(32, 25, activation=tf.nn.leaky_relu, strides=2),
-            layers.Conv1D(1, 25, activation=tf.nn.leaky_relu, strides=2),
-            layers.MaxPool1D(3, 1)
+            layers.InputLayer(input_shape=(224,224,3)),
+            layers.Conv2D(16, (3,3), 1, padding="same", activation="relu"),
+            layers.MaxPool2D((2,2), padding="same", strides=2),
+            layers.Conv2D(8, (3,3), 1, padding="same", activation="relu"),
+            layers.MaxPool2D((2,2), padding="same", strides=2),
+            layers.Conv2D(8, (3,3), 1, padding="same", activation="relu"),
+            layers.MaxPool2D((2,2), padding="same", strides=2),
+            layers.Flatten()
+        ])
+        self.decoder = tf.keras.Sequential ([
+            layers.InputLayer(input_shape=(6272)),
+            layers.Reshape((28,28,8)),
+            layers.Conv2DTranspose(8, (3,3), 2, padding="same", activation="relu"),
+            layers.Conv2DTranspose(8, (3,3), 2, padding="same", activation="relu"),
+            layers.Conv2DTranspose(16, (3,3), 2, padding="same", activation="relu"),
+            layers.Conv2D(3, (3,3), 1, padding="same", activation="sigmoid")
         ])
 
     def call(self, x):
-        return self.encoder(x)
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
 
 autoencoder = GalaxyEncoder()
 autoencoder.compile()
 print(autoencoder.encoder.summary())
+print(autoencoder.decoder.summary())

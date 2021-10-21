5b8d82fd54da727775b7364bf69377e3c5e7f8b9
Ezra Fielding <ezra.fielding@gmail.com>
2021-10-21 12:21:33 +0200

    Added Upsampling

--- unsupervised_learning/galaxyencode.py
+++ unsupervised_learning/galaxyencode.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras import layers
+from tensorflow.python.keras.layers.convolutional import UpSampling2D
 
 class GalaxyEncoder(Model):
     def __init__(self):
@@ -18,9 +19,12 @@ class GalaxyEncoder(Model):
         self.decoder = tf.keras.Sequential ([
             layers.InputLayer(input_shape=(6272)),
             layers.Reshape((28,28,8)),
-            layers.Conv2DTranspose(8, (3,3), 2, padding="same", activation="relu"),
-            layers.Conv2DTranspose(8, (3,3), 2, padding="same", activation="relu"),
-            layers.Conv2DTranspose(16, (3,3), 2, padding="same", activation="relu"),
+            layers.UpSampling2D((2,2)),
+            layers.Conv2DTranspose(8, (3,3), 1, padding="same", activation="relu"),
+            layers.UpSampling2D((2,2)),
+            layers.Conv2DTranspose(8, (3,3), 1, padding="same", activation="relu"),
+            layers.UpSampling2D((2,2)),
+            layers.Conv2DTranspose(16, (3,3), 1, padding="same", activation="relu"),
             layers.Conv2D(3, (3,3), 1, padding="same", activation="sigmoid")
         ])
 
